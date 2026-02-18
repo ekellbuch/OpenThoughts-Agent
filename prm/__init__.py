44244bdc0e993871db07dca968b9ea04cd9f9e76
Benjamin Feuer <penfever@gmail.com>
2026-02-18 17:01:49 -0500

    tweak to jupiter config

--- prm/__init__.py
+++ prm/__init__.py
@@ -7,10 +7,14 @@ from prm.base import (
     register_prm,
 )
 
+# Import PRM implementations so @register_prm decorators execute
+from prm.thrashing_detector import ThrashingDetector  # noqa: F401
+
 __all__ = [
     "PRMJudgment",
     "ProcessRewardModel",
     "Trajectory",
+    "ThrashingDetector",
     "get_prm",
     "list_prms",
     "register_prm",
